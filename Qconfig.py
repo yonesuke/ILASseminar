b6e3205fd8111f4ee5bbc913eddb6644c539d60b
yonesuke <13e.e.c.13@gmail.com>
2018-07-25 18:38:07 +0900

    Hide my IBM Q API token

--- Qconfig.py
+++ Qconfig.py
@@ -3,7 +3,7 @@
 # access token. Replace "None" below with the quoted token string.
 # Uncomment the APItoken variable, and you will be ready to go.
 
-APItoken = "e59359d7d79d74905edd7667e7ab717f505f0291fa2f46a7f69d8a778476188698a52701111acf2859e63ee9d3cbd28149c2680722714e9f5a20dd92d76476ab"
+APItoken = "higehige"
 
 config = {
   "url": 'https://quantumexperience.ng.bluemix.net/api'
